5c40669ecda7c0dcfeea309e39b8eb9f798f3e15
rAthenaCN <3438333518@qq.com>
2022-06-08 17:58:40 +0800

    [纠正] 文件数量与 rAthena 保持一致

--- 3rdparty/rapidyaml/ext/c4core/src/c4/ext/fast_float_all.h
+++ 3rdparty/rapidyaml/ext/c4core/src/c4/ext/fast_float_all.h
@@ -1,5 +1,5 @@
 // fast_float by Daniel Lemire
-// fast_float by Jo?o Paulo Magalhaes
+// fast_float by João Paulo Magalhaes
 //
 // with contributions from Eugene Golushkov
 // with contributions from Maksim Kita
@@ -750,11 +750,11 @@ namespace fast_float {
  */
 
 /**
- * The smallest non-zero float (binary64) is 2^?1074.
+ * The smallest non-zero float (binary64) is 2^−1074.
  * We take as input numbers of the form w x 10^q where w < 2^64.
  * We have that w * 10^-343  <  2^(64-344) 5^-343 < 2^-1076.
  * However, we have that
- * (2^64-1) * 10^-342 =  (2^64-1) * 2^-342 * 5^-342 > 2^?1074.
+ * (2^64-1) * 10^-342 =  (2^64-1) * 2^-342 * 5^-342 > 2^−1074.
  * Thus it is possible for a number of the form w * 10^-342 where
  * w is a 64-bit value to be a non-zero floating-point number.
  *********
